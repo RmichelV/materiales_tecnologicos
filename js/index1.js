aaf73bb9de39729d6a76b7dd1b44e13c1003419e
Richard Villalba <richard.villalba.mendieta@gmail.com>
2022-11-17 16:17:37 -0400

    fin

--- js/index1.js
+++ js/index1.js
@@ -10,3 +10,17 @@ function materia(){
 function video(){
     location.href="../html/index3.html"
 }
+let nom,ape,em;
+
+function valores(){
+    let nombre = document.getElementById("nombre");
+    nom= nombre.value;
+    let apellido = document.getElementById("apellido");
+    ape=apellido.value;
+    let email= document.getElementById("email");
+    em=email.value;
+
+    document.write("GRACIAS POR TUS DATOS "+nom +" "+ape +" pronto te enviaremos un correo a: "+em+" ten un lindo dia :D ")
+
+    
+}
